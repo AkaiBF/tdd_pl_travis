8908772de943efd3dd99c75499018097d9aec3a2
Ernesto Echeverría <alu0100881622@ull.edu.es>
2017-10-25 11:22:21 +0000

    Fixed to_s

--- lib/alimento.rb
+++ lib/alimento.rb
@@ -8,7 +8,9 @@ class Alimento
    end
    
    def to_s() 
-       return String.new(@name + ", " + @proteins + "g proteins, " + @glucids + "g glucids, " + @lipids + "g lipids")
+       return String.new(@name + ", " + @proteins.to_s + "g proteins, " + @glucids.to_s + "g glucids, " + @lipids.to_s + "g lipids")
    end
    
 end
+
+Alimento.new("cerdo", 12, 10, 9).to_s
