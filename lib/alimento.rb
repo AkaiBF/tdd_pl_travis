63c7107a2adad56d05a60371088ee10b942c1447
Ernesto Echeverría <alu0100881622@ull.edu.es>
2017-10-25 08:56:53 +0000

    Dev up to date

--- lib/alimento.rb
+++ lib/alimento.rb
@@ -1,6 +1,6 @@
 class Alimento
     attr_reader :name
-   def initialize(name)
+   def initialize (name)
        @name = name
    end
 end
--- lib/nutrientes/version.rb
+++ lib/nutrientes/version.rb
@@ -1,3 +1,3 @@
 module Nutrientes
-  VERSION = "0.1.0"
+  VERSION = "1.0.1"
 end
