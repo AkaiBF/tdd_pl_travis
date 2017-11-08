3394a29ad6d461f26036180b82b60ab3de05d3bc
Ernesto Echeverría <alu0100881622@ull.edu.es>
2017-11-08 09:40:17 +0000

    Node on Alimento

--- lib/alimento.rb
+++ lib/alimento.rb
@@ -1,3 +1,11 @@
+class Node 
+    @@estructura = Struct.new(:value, :next_, :prev_)
+    attr_reader :nodo
+    def initialize(val) 
+        @nodo = @@estructura.new(val, nil, nil)
+    end
+end
+
 class Alimento
     attr_reader :name, :proteins, :glucids, :lipids
    def initialize (name, proteins, glucids, lipids)
