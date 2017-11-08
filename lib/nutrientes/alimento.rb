61da9ff9a9b725045b60090273e484cad15d3b28
Ernesto Echeverría <alu0100881622@ull.edu.es>
2017-11-08 12:20:52 +0000

    Structure fixed

--- lib/nutrientes/alimento.rb
+++ lib/nutrientes/alimento.rb
@@ -1,69 +1,3 @@
-class Node 
-    @@estructura = Struct.new(:value, :next_, :prev_)
-    attr_reader :nodo
-    def initialize(val) 
-        @nodo = @@estructura.new(val, nil, nil)
-    end
-end
-
-class List
-    attr_reader :head, :tail, :size
-    def initialize()
-        @size = 0
-        @head = nil
-        @tail = nil
-    end
-    
-    def push(nodo) 
-        if @size == 0 then
-            @head = nodo
-            @tail = nodo
-        else
-            @tail.nodo[:next_] = nodo
-            nodo.nodo[:prev_] = @tail
-            @tail = nodo
-        end
-        @size += 1
-    end
-    
-    def insert(nodo)
-        if @size == 0 then
-            @head = nodo
-            @tail = nodo
-        else
-            @head.nodo[:prev_] = nodo
-            nodo.nodo[:next_] = @head
-            @head = nodo
-        end
-        @size += 1
-    end
-    
-    def truncate
-        if size > 1
-            @head = @head.nodo[:next_]
-            @head.nodo[:prev_] = nil
-            @size -= 1
-        elsif size == 1
-            @head = nil
-            @tail = nil
-            @size -= 1
-        end
-    end
-    
-    def pop
-        if size > 1
-            @tail = @tail.nodo[:prev_]
-            @tail.nodo[:next_] = nil
-            @size -= 1
-        elsif size == 1
-            @tail = nil
-            @head = nil
-            @size -= 1
-        end
-    end
-end
-    
-
 class Alimento
     attr_reader :name, :proteins, :glucids, :lipids
    def initialize (name, proteins, glucids, lipids)
@@ -84,6 +18,12 @@ class Alimento
 end
 
 class OvoLacteo < Alimento
+    def initialize(name, proteins, glucids, lipids)
+        super(name, proteins, glucids, lipids)
+    end
+end
+
+class Carnido < Alimento
     def initialize(name, proteins, glucids, lipids)
         super(name, proteins, glucids, lipids)
     end
