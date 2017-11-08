595bfb3aaacc9a864cc0936a429bb82676bc529f
Ernesto Echeverría <alu0100881622@ull.edu.es>
2017-11-08 10:13:15 +0000

    Test on list1

--- spec/lista_spec.rb
+++ spec/lista_spec.rb
@@ -2,10 +2,19 @@ require "spec_helper"
 
 RSpec.describe Node do
     before(:all) do
-       @Node1 = Node.new(3)
+       @node1 = Node.new(3)
     end
     
     it "has a value" do
-        expect(@Node1.nodo[:value]).not_to be nil
+        expect(@node1.nodo[:value]).not_to be nil
     end
 end
+
+RSpec.describe List do
+    before(:all) do
+        @lista = List.new()
+        @node1 = Node.new(3)
+        @lista.push(@node1)
+    end
+    
+    
